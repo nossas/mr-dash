dfa72ce46e8343954467b10c38dcd48e23af7073
Nícolas Iensen <nicolas@engage.is>
2012-06-20 09:42:07 -0300

    #1 Refactor tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe User do
   describe ".sync_with_meurio" do
-    before { stub_request(:get, "http://meurio.org.br/members.json?page=1&token=HelloWorld").to_return(:body => File.open("#{Rails.root}/features/support/mr_members1.json").read) }
-    before { stub_request(:get, "http://meurio.org.br/members.json?page=2&token=HelloWorld").to_return(:body => File.open("#{Rails.root}/features/support/mr_members2.json").read) }
-    before { stub_request(:get, "http://meurio.org.br/members.json?page=3&token=HelloWorld").to_return(:body => File.open("#{Rails.root}/features/support/mr_members3.json").read) }
+    let(:page1_url){ "http://meurio.org.br/members.json?page=1&token=HelloWorld" }
+    let(:page2_url){ "http://meurio.org.br/members.json?page=2&token=HelloWorld" }
+    let(:page3_url){ "http://meurio.org.br/members.json?page=3&token=HelloWorld" }
     context "when there is 10 members in 2 pages" do
+      before { stub_request(:get, page1_url).to_return(:body => File.open("#{Rails.root}/features/support/mr_members1.json").read) }
+      before { stub_request(:get, page2_url).to_return(:body => File.open("#{Rails.root}/features/support/mr_members2.json").read) }
+      before { stub_request(:get, page3_url).to_return(:body => File.open("#{Rails.root}/features/support/mr_members3.json").read) }
       it "should make a request to the first page" do
         User.sync_with_meurio
         WebMock.should have_requested(:get, "http://meurio.org.br/members.json").with(:query => {:token => ENV["DASH_TOKEN"], :page => 1})
