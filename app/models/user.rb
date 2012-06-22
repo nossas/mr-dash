9c2fee226780b46db1fbca105a53f15ef5c869a3
Nícolas Iensen <nicolas@engage.is>
2012-06-22 17:11:09 -0300

    #1 sync_with_meurio refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,30 @@
 class User < ActiveRecord::Base
-  include Mailee
-
   attr_accessible :avatar_url, :celular, :email, :first_name, :last_name, :registered_at
   validates :email, :uniqueness => true
 
   def self.sync_with_meurio
-    page = 1
     last_sync = Sync.where(:name => "User.sync_with_meurio").order("created_at DESC").limit(1).first
     Sync.create :name => "User.sync_with_meurio"
-    while true do
-      members = JSON.parse(HTTParty.get("http://meurio.org.br/members.json", :query => {:token => ENV["DASH_TOKEN"], :page => page, :by_updated_at => last_sync ? last_sync.created_at : ""}).body)
+    members = self.get_meurio_members(page = 1, last_sync ? last_sync.created_at : nil)
+    while members.any? do
       members.each do |member|
-        user = User.find_by_email(member["email"])
         puts "Syncing page ##{page} #{member['email']}"
-        if user
-          user.update_attributes(
-            :first_name => member["first_name"],
-            :last_name => member["last_name"],
-            :email => member["email"],
-            :celular => member["celular"],
-            :registered_at => member["created_at"],
-            :avatar_url => member["image_url"]
-          )
-        else
-          user = User.create(
-            :first_name => member["first_name"],
-            :last_name => member["last_name"],
-            :email => member["email"],
-            :celular => member["celular"],
-            :registered_at => member["created_at"],
-            :avatar_url => member["image_url"]
-          )
-        end
-        Contact.create(:name => user.first_name, :email => user.email, :list => "[Mr. Dash] Meu Rio")
+        user = User.find_or_create_by_email(
+          member["email"], 
+          :first_name => member["first_name"],
+          :last_name => member["last_name"],
+          :email => member["email"],
+          :celular => member["celular"],
+          :registered_at => member["created_at"],
+          :avatar_url => member["image_url"]
+        )
       end
-      break if members.empty?
-      page += 1
+      members = self.get_meurio_members(page += 1, last_sync ? last_sync.created_at : nil)
     end
+  end
+
+  def self.get_meurio_members page = 1, by_updated_at = nil
+    JSON.parse(HTTParty.get("http://meurio.org.br/members.json", :query => {:token => ENV["DASH_TOKEN"], :page => page, :by_updated_at => by_updated_at}).body)
   end
 
   def name
